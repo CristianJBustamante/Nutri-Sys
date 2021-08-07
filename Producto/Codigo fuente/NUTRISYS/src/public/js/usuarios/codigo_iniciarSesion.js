d37a6ce71f92c94dfaac0131fc9f5fca7b465860
serenaprado <serenaiprado@gmail.com>
2021-08-07 09:30:28 -0300

    login

--- Producto/Codigo fuente/NUTRISYS/src/public/js/usuarios/codigo_iniciarSesion.js
+++ Producto/Codigo fuente/NUTRISYS/src/public/js/usuarios/codigo_iniciarSesion.js
@@ -1,17 +1,33 @@
+
 function validarUsuario() {
-    const fetch = require('node-fetch');
-    var usu_usuario = document.getElementById("usu_usuario").value;
-    var usu_clave = document.getElementById("usu_clave").value;
+    var usu_usuario = document.getElementById("usuario").value;
+    var usu_clave = document.getElementById("clave").value;
     var url = "http://192.168.0.188:3000/usuario/"+usu_usuario+'/'+usu_clave
-    alert(url)
     fetch(url)
-    .then(res => res.json())
-    .then(json => {
-        console.log("First user in the array:");
-        console.log(json[0]);
-});
-        alert("Paciente registrado con éxito")
-}
+        .then(response => response.json())
+        .then(data => mostrarData(data))
+        .catch(error => console.log(error))
 
+      const mostrarData = (data) => {
+        console.log(data)
+        if (data.length==0){
+          swal("Atención","Usuario no válido","error") 
+        }
+        else {
+                swal("Bienvenido/a",data[0].apellido + ", " + data[0].nombre,"success")
+                .then((value) => {
+                        location.href ="../pacientes/buscarpaciente"})
+                
+                
+        }
+        
+}}
 
-
+function mostrarClave(){
+        var tipo = document.getElementById("clave");
+        if(tipo.type == "password"){
+            tipo.type = "text";
+        }else{
+            tipo.type = "password";
+        }
+    }

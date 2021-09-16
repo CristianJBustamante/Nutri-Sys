74b4ed6739a43599d82e72eb4d6da8ed3792c765
Nahuel <nahuelmonzon93@gmail.com>
2021-09-16 17:57:54 -0300

    habitos
    Investigacion sobre como agregar los habitos seleccionados a la grilla de seleccionados

---
+++ Producto/Programacion/js/codigo_habitosNoSaludables.js
@@ -0,0 +1,8 @@
+function seleccionar(){
+let checkbox = document.getElementById('check_1')
+  if (checkbox.checked === true) {
+    alert("habito seleccionado");
+    checkbox.style.border= "solid var(--blanco)";
+  } 
+}
+

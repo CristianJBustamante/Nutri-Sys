3015f02f9b8da74b4036c822a99ce6061a1f93ff
Nahuel <nahuelmonzon93@gmail.com>
2021-09-16 21:13:26 -0300

    habitos
    pequeños cambios estéticos

--- Producto/Programacion/js/codigo_habitosNoSaludables.js
+++ Producto/Programacion/js/codigo_habitosNoSaludables.js
@@ -2,7 +2,6 @@ function seleccionar(){
 let checkbox = document.getElementById('check_1')
   if (checkbox.checked === true) {
     alert("habito seleccionado");
-    checkbox.style.border= "solid var(--blanco)";
   } 
 }
 

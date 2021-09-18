5f3ef37424ebc280adb913c3513b46ebd0c4dc8c
Nahuel <nahuelmonzon93@gmail.com>
2021-09-18 12:49:06 -0300

    habitos
    investicación para mover filas a otra tabla

--- Producto/Programacion/js/codigo_habitosNoSaludables.js
+++ Producto/Programacion/js/codigo_habitosNoSaludables.js
@@ -1,7 +1,14 @@
-function seleccionar(){
-let checkbox = document.getElementById('check_1')
-  if (checkbox.checked === true) {
-    alert("habito seleccionado");
-  } 
-}
-
+// function seleccionar(){
+// let checkbox = document.getElementById('check_1')
+//   if (checkbox.checked === true) {
+//     alert("habito seleccionado");
+//   } 
+// }
+$(document).ready(function(){
+$('.envio').on('click', function() {
+  var hermano=$(this).parent();
+  $("#tabla2").prepend("<tr>\
+                            <td>"+hermano.siblings("td:eq(0)").text()+"</td>\
+                            </tr>")
+});
+});

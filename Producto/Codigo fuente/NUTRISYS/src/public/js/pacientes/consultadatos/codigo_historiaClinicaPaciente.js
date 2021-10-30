c079c723be57624fe71f96a7dd66b311ac5b5925
Nahuel <nahuelmonzon93@gmail.com>
2021-10-30 20:02:24 -0300

    commit
    se quitó el campo nacionalidad en la pantalla resumen

--- Producto/Codigo fuente/NUTRISYS/src/public/js/pacientes/consultadatos/codigo_historiaClinicaPaciente.js
+++ Producto/Codigo fuente/NUTRISYS/src/public/js/pacientes/consultadatos/codigo_historiaClinicaPaciente.js
@@ -19,7 +19,7 @@ const mostrarData = (data) => {
     cabecera += `<h2>Paciente: ${data.pac_apellido}, ${data.pac_nombre}</h2><h2>HC: ${data.pac_nrohc}</h2><h2>DOC.: ${data.pac_nrodoc}</h2><h2>FN: ${data.pac_fechanac}</h2>`        
     document.getElementById('pac_datos').innerHTML = cabecera
     let resumen = ''
-    resumen += `<h3>Nacionalidad:</h3><h3>Argentina</h3><h3>Domicilio:</h3><h3>${data.pac_direccion}</h3><h3>Localidad / Provincia:</h3><h3>Córdoba / Córdoba</h3><h3>Profesión:</h3><h3>${data.hc_ocupacion}</h3><h3>Celular:</h3><h3>${data.pac_telefono1}</h3><h3>Correo:</h3><h3>${data.pac_correo}</h3>`
+    resumen += `<h3>Domicilio:</h3><h3>${data.pac_direccion}</h3><h3>Localidad / Provincia:</h3><h3>Córdoba / Córdoba</h3><h3>Profesión:</h3><h3>${data.hc_ocupacion}</h3><h3>Celular:</h3><h3>${data.pac_telefono1}</h3><h3>Correo:</h3><h3>${data.pac_correo}</h3>`
     document.getElementById('pac_datospersonales').innerHTML = resumen
     }
     

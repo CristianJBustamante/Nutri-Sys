dd0adfb6408230d3b261b72c1e8b8a5ae7fbdc34
JaviB364 <66915056+JaviB364@users.noreply.github.com>
2021-12-12 19:33:46 -0300

    Registro de plan alimentario.
    Completo parte frontend registro de plan alimentario.

--- Producto/Programacion/Plantilla/assets/js/planAlimentario.js
+++ Producto/Programacion/Plantilla/assets/js/planAlimentario.js
@@ -1,5 +1,8 @@
+var comida;
+var titulo;
+var descripcion;
 
-let cerrar = document.querySelectorAll(".cerrar")[0];
+let cerrar = document.querySelectorAll(".cerrar");
 let abrir = document.querySelectorAll(".cta");
 let modal = document.querySelectorAll(".modales")[0];
 let modalC = document.querySelectorAll(".modales-container")[0];
@@ -7,19 +10,21 @@ let modalC = document.querySelectorAll(".modales-container")[0];
 abrir.forEach(item =>{
     item.addEventListener("click", function(e){
         e.preventDefault();
-        console.log("OK");
         modalC.style.opacity = "1";
         modalC.style.visibility = "visible";
         modal.classList.toggle("modales-close");
+        traerDatosComida();
 })
 })
 
-cerrar.addEventListener("click",function(e){
-    modal.classList.toggle("modales-close");
-    setTimeout(function(){
-        modalC.style.opacity = "0";
-        modalC.style.visibility = "hidden";
-    }, 900);
+cerrar.forEach(item =>{
+    item.addEventListener("click", function(e){
+        modal.classList.toggle("modales-close");
+        setTimeout(function(){
+            modalC.style.opacity = "0";
+            modalC.style.visibility = "hidden";
+        }, 900);
+})
 })
 
 window.addEventListener("click", function(e){
@@ -31,3 +36,41 @@ window.addEventListener("click", function(e){
     }, 900);
     }
 })
+
+function comidaAEditar(comidaEditar){
+    comida = comidaEditar;
+}
+
+function guardarComida(){
+    // console.log(document.getElementById("tituloComida").value);
+    // var nodo = "<h2>" + document.getElementById("tituloComida").value + "</h2>";
+    // console.log(nodo);
+    // document.getElementById(comida).appendChild(nodo);
+    // document.getElementById(comida).innerHTML = nodo;
+
+    var capa = document.getElementById(comida);
+    var tituloComida = document.createElement("h3");
+    var descripcionComida = document.createElement("h3");
+    tituloComida.innerHTML = document.getElementById("tituloComida").value;
+    descripcionComida.innerHTML = document.getElementById("descripcionComida").value;
+    descripcionComida.style.display = "none";
+    capa.innerHTML = "";
+    capa.appendChild(tituloComida);
+    capa.appendChild(descripcionComida);
+}
+
+function traerDatosComida(){
+    var comidaDatos = document.getElementById(comida);
+    var tbxTitulo = document.getElementById("tituloComida");
+    var tbxDescripcion = document.getElementById("descripcionComida");
+    tbxTitulo.value="";
+    tbxDescripcion.value="";
+    // console.log(comidaDatos.firstChild.innerText);
+    // console.log(comidaDatos.lastChild.innerText);
+    if(comidaDatos.firstChild.innerText != undefined){
+        tbxTitulo.value = comidaDatos.firstChild.innerText;
+    }
+    if(comidaDatos.lastChild.innerText != undefined){
+        tbxDescripcion.value = comidaDatos.lastChild.innerText;
+    }
+}
